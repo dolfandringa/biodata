2f23b74dfede784c7fcc25cb1dadc3597bd575a2
Dolf Andringa <dolfandringa@gmail.com>
2015-06-01 16:10:45 +0800

    Removed rvc_parrotfish_et_al dataset

--- model/__init__.py
+++ model/__init__.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from configparser import SafeConfigParser
 import rvc_species
-import rvc_parrotfish_et_al
 import random_swim_species
 config=SafeConfigParser()
 config.read('settings.cfg')
 uri=config.get('database','uri')
 engine = create_engine(uri)
 
-datasets = [rvc_species, random_swim_species, rvc_parrotfish_et_al]
+datasets = [rvc_species, random_swim_species]
 
 if __name__ == "__main__":
     from sqlalchemy.ext.declarative import declarative_base
